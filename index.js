2afb456557641c5c50c5f2aa7ff91316560abe19
Bruno M. Cerqueira <brunocerqueira@recbasvr.com.br>
2019-03-11 20:33:30 -0300

    Variváveis e Seletores

---
+++ index.js
@@ -0,0 +1,8 @@
+var nome        = document.querySelector('#exampleInputName');
+var genero      = document.querySelectorAll("#form-user-create [name=gender]:checked");
+var nascimento  = document.querySelector('#exampleInputBirth');
+var pais        = document.querySelector('#exampleInputCountry');
+var email       = document.querySelector('#exampleInputEmail');
+var senha       = document.querySelector('#exampleInputPassword');
+var foto        = document.querySelector('#exampleInputFile');
+var admin       = document.querySelector('#exampleInputAdmin');
