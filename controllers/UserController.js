52a62fd5ef5df404c7c9642ee4c933dfe127dcfa
Bruno Romeo <31778416+brunoromeo001@users.noreply.github.com>
2019-03-21 17:33:10 -0300

    Add files via upload

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -57,8 +57,15 @@ class UserController{
 
                 reject(e);
             };
+
+            if(file){
+
+                fileReader.readAsDataURL(file);
+            } else{
+                
+                resolve('dist/img/boxed-bg.jpg');
+            }
             
-            fileReader.readAsDataURL(file);
         });
         
     }
@@ -75,7 +82,11 @@ class UserController{
     
                     user[field.name] = field.value;
                 }
-            }else{
+            }else if(field.name == "admin"){
+
+                user[field.name] == field.checked;
+            }
+            else{
     
                 user[field.name] = field.value;
             }
@@ -94,18 +105,23 @@ class UserController{
 
     addLine(dataUser){ 
 
-        this.tableEl.innerHTML = `
+        let tr = document.createElement('tr');
+
+        tr.innerHTML = `
             <tr>
                 <td><img src="${dataUser.photo}" alt="User Image" class="img-circle img-sm"></td>
                 <td>${dataUser.name}</td>
                 <td>${dataUser.email}</td>
-                <td>${dataUser.admin}</td>
+                <td>${(dataUser.admin) ? 'Sim' : 'Não'}</td>
                 <td>${dataUser.birth}</td>
                 <td>
-                     <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
+                    <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
                     <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
                 </td>
-            </tr>  
-        `;    
+            </tr>          
+        `;
+
+        this.tableEl.appendChild(tr);            
+        
     }
 }
