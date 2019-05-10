783c30ec5c74b79b4c5ed3c2b1d996a972d57445
Bruno M. Cerqueira <brunocerqueira@recbasvr.com.br>
2019-05-10 05:44:37 -0300

    Validando Formulário

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -81,8 +81,16 @@ class UserController{
     getValues(){
 
         let user = {};
+        let isValid = true;
                 
         [...this.formEl.elements].forEach(function(field, index){
+
+            if (['name', 'email', 'password'].indexOf(field.name) > -1 && !field.value){
+
+                field.parentElement.classList.add("has-error");
+                
+                isValid = false;
+            }
 
             if(field.name == "gender"){
      
@@ -99,7 +107,12 @@ class UserController{
                 user[field.name] = field.value;
             }
         });
-    
+        
+        if (!isValid){
+
+            return false;
+        }
+
         return new User(
             user.name, 
             user.gender, 
