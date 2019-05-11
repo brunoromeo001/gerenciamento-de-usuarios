ad7ffb222cc58758f21155fd76e54df83f6827d3
Bruno M. Cerqueira <brunocerqueira@recbasvr.com.br>
2019-05-11 10:39:25 -0300

    Atualizando Estatísticas - Usando dataset

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -20,6 +20,8 @@ class UserController{
 
             let values = this.getValues();
 
+            if (!values) return false;
+
             // Arrow Function quando subistitui o (funciton) por ( => ) 
                 this.getPhoto().then(
                 (content) => {
@@ -129,6 +131,8 @@ class UserController{
 
         let tr = document.createElement('tr');
 
+        tr.dataset.user = JSON.stringify(dataUser);
+
         tr.innerHTML = `
             <tr>
                 <td><img src="${dataUser.photo}" alt="User Image" class="img-circle img-sm"></td>
@@ -143,7 +147,28 @@ class UserController{
             </tr>          
         `;
 
-        this.tableEl.appendChild(tr);            
+        this.tableEl.appendChild(tr);    
         
+        this.updateCount();
+        
+    }
+
+    updateCount(){
+
+        let numberUsers = 0;
+        let numberAdmin = 0;
+
+        [...this.tableEl.children].forEach(tr=>{
+
+            numberUsers++;
+
+            let user = JSON.parse(tr.dataset.user);
+
+            if (user._admin) numberAdmin++;
+
+        });
+
+        document.querySelector("#number-users").innerHTML = numberUsers;
+        document.querySelector("#number-users-admin").innerHTML = numberAdmin;
     }
 }
