0c744d6d05ffff7feaf00a8942075ab9fcd02747
Bruno M. Cerqueira <brunocerqueira@recbasvr.com.br>
2019-06-01 19:09:57 -0300

    Refactoring - Otimizando o Template

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -48,23 +48,11 @@ class UserController{
 
                     }
 
-                    tr.dataset.user = JSON.stringify(result);
+                    let user = new User();
 
-                    tr.innerHTML = `
-                        <tr>
-                            <td><img src="${result._photo}" alt="User Image" class="img-circle img-sm"></td>
-                            <td>${result._name}</td>
-                            <td>${result._email}</td>
-                            <td>${(result._admin) ? 'Sim' : 'Não'}</td>
-                            <td>${Utils.dateFormat(result._register)}</td>
-                            <td>
-                                <button type="button" class="btn btn-primary btn-xs btn-flat btn-edit">Editar</button>
-                                <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
-                            </td>
-                        </tr>          
-                    `;
-
-                    this.addEventsTr(tr);
+                    user.loadFromJSON(result);
+                    
+                    this.getTr(user, tr);                   
 
                     this.updateCount();
 
@@ -244,31 +232,39 @@ class UserController{
 
     addLine(dataUser){ 
 
-        let tr = document.createElement('tr');
-        
-        tr.dataset.user = JSON.stringify(dataUser);
-
-        tr.innerHTML = `
-            <tr>
-                <td><img src="${dataUser.photo}" alt="User Image" class="img-circle img-sm"></td>
-                <td>${dataUser.name}</td>
-                <td>${dataUser.email}</td>
-                <td>${(dataUser.admin) ? 'Sim' : 'Não'}</td>
-                <td>${Utils.dateFormat(dataUser.register)}</td>
-                <td>
-                    <button type="button" class="btn btn-primary btn-xs btn-flat btn-edit">Editar</button>
-                    <button type="button" class="btn btn-danger btn-delete btn-xs btn-flat">Excluir</button>
-                </td>
-            </tr>          
-        `;
-        
-        this.addEventsTr(tr);
-
+        let tr = this.getTr(dataUser);
+                                              
         this.tableEl.appendChild(tr);    
         
         this.updateCount();
         
     }
+    
+    getTr(dataUser, tr = null){
+        
+        if (tr === null) tr = document.createElement('tr');
+
+        tr.dataset.user = JSON.stringify(dataUser);
+        
+        tr.innerHTML = `
+            <tr>
+            <td><img src="${dataUser.photo}" alt="User Image" class="img-circle img-sm"></td>
+            <td>${dataUser.name}</td>
+            <td>${dataUser.email}</td>
+            <td>${(dataUser.admin) ? 'Sim' : 'Não'}</td>
+            <td>${Utils.dateFormat(dataUser.register)}</td>
+            <td>
+                <button type="button" class="btn btn-primary btn-xs btn-flat btn-edit">Editar</button>
+                <button type="button" class="btn btn-danger btn-delete btn-xs btn-flat">Excluir</button>
+            </td>
+            </tr>          
+        `;
+
+        this.addEventsTr(tr);
+
+        return tr;
+    }
+    
 
     addEventsTr(tr){
 
